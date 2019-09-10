e2026fc44dde7d0119fdba20dcc10fa5de24e335
chli9541 <chli9541@uni.sydney.edu.au>
2019-09-11 01:52:32 +1000

    Update train_and_predict.py

--- schema_guided_dst/baseline/train_and_predict.py
+++ schema_guided_dst/baseline/train_and_predict.py
@@ -69,7 +69,7 @@ flags.DEFINE_integer("predict_batch_size", 8, "Total batch size for predict.")
 
 flags.DEFINE_float("learning_rate", 1e-4, "The initial learning rate for Adam.")
 
-flags.DEFINE_float("num_train_epochs", 80.0,
+flags.DEFINE_float("num_train_epochs", 1.0,
                    "Total number of training epochs to perform.")
 flags.DEFINE_float(
     "warmup_proportion", 0.1,
